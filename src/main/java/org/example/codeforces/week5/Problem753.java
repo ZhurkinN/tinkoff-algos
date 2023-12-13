d631b66fc7d9fd70bd6ae3cc1f50ab59a7b9e491
n.s.zhurkin <n.s.zhurkin@tinkoff.ru>
2023-12-14 01:41:11 +0300

    add: week 5, fix 1 task

--- src/main/java/org/example/codeforces/week5/Problem753.java
+++ src/main/java/org/example/codeforces/week5/Problem753.java
@@ -1,5 +1,8 @@
 package org.example.codeforces.week5;
 
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Problem753 {
@@ -9,6 +12,23 @@ public class Problem753 {
         int n = scanner.nextInt();
         scanner.close();
 
+        List<Integer> results = new ArrayList<>();
+        int currentCandy = 1;
+        int result = 0;
+        while (result + currentCandy <= n) {
+            results.add(currentCandy);
+            result += currentCandy++;
+        }
+        int diff = n - result;
+        if (diff > 0) {
+            int last = results.get(results.size() - 1);
+            results.remove(results.size() - 1);
+            results.add(last + diff);
+        }
 
+        System.out.println(results.size());
+        for (int i : results) {
+            System.out.print(i + " ");
+        }
     }
 }
